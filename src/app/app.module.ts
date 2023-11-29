93594bbde73a4c6c11a18827517173c7d8755da8
junaid <junaidvinu133@gmail.com>
2023-11-29 12:01:32 +0530

    update ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { SignupComponent } from './components/user/signup/signup.component';
+import { NavBarComponent } from './components/user/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,

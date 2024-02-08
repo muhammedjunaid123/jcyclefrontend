52e3e8dc2e4ceff671f91504e3b919a92ce10871
junaid <junaidvinu133@gmail.com>
2024-02-08 16:13:45 +0530

    environment update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,10 @@
 export const environment = {
     production: true,
-    APIuRL:'http://localhost:3000',
+    APIuRL:'https://jcycle.shop',
+    UserSecret:'JwtUserJcycle',
+    AdminSecrect:'JwtadminJcycle',
+    servicerSecret:'JwtServicerJcycle',
+    RAZOR_KEY:'rzp_test_D85C37TIl3xCuv',
+    socketEndPoint:'https://jcycle.shop'
    
 };

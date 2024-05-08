85b87691af318f1bea957f438dc4726d7d74d0e0
junaid <junaidvinu133@gmail.com>
2024-05-08 12:44:33 +0530

    added new url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    APIuRL:'https://jcycle.shop',
+    APIuRL:'https://jcyclebackend.onrender.com',
     UserSecret:'JwtUserJcycle',
     AdminSecrect:'JwtadminJcycle',
     servicerSecret:'JwtServicerJcycle',
     RAZOR_KEY:'rzp_test_8FA0gTterPQSaY',
-    socketEndPoint:'https://jcycle.shop'
+    socketEndPoint:'https://jcyclebackend.onrender.com'
 };
